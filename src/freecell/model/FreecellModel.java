0a512cd7d721677bcaa5023779640206f4caf80b
Rujun Yao <Yaufaaling@Rujuns-MacBook-Pro.local>
2018-10-22 21:24:13 -0700

    Temporary implementation of some utility functions.

--- src/freecell/model/FreecellModel.java
+++ src/freecell/model/FreecellModel.java
@@ -1,10 +1,30 @@
 package freecell.model;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FreecellModel implements FreecellOperations {
+
+  private final int CARDNUM = 52;
+  private final int CARDTYPENUM = 13;
+
+  // Still confusing about what this function do, so I suppose it returns a new deck.
+
   public List getDeck() {
-    return null;
+
+    List Deck = new ArrayList();
+
+    for (int i = 0; i < CARDTYPENUM; i++) {
+      Deck.add(new Card(CardType.SPADES, i + 1));
+      Deck.add(new Card(CardType.DIAMONDS, i + 1));
+      Deck.add(new Card(CardType.HEARTS, i + 1));
+      Deck.add(new Card(CardType.CLUBS, i + 1));
+    }
+
+    shuffle(Deck);
+
+    return Deck;
   }
 
   public void startGame(List deck, boolean shuffle) throws IllegalArgumentException {
@@ -21,6 +41,16 @@ public class FreecellModel implements FreecellOperations {
 
   public String getGameState() {
     return null;
+  }
+
+  private void shuffle(List deck) {
+    Collections.shuffle(deck);
+  }
+
+  private boolean isDeckValid(List deck) {
+
+    // To be implemented.
+    return false;
   }
 
 }

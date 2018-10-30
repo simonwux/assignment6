5d413d2709c674fa42df3b2c0b0c0d3f2ac63614
pandalv9999 <pandalv9999@gmail.com>
2018-10-29 21:32:21 -0700

    Add implementation of testMove()

--- test/FreecellModelTest.java
+++ test/FreecellModelTest.java
@@ -99,21 +99,125 @@ public class FreecellModelTest {
             "C5: 2♠, 4♠, 6♠, 8♠, 10♠, Q♠\n" +
             "C6: 2♦, 4♦, 6♦, 8♦, 10♦, Q♦\n" +
             "C7: 2♥, 4♥, 6♥, 8♥, 10♥, Q♥\n" +
-            "C8: 2♣, 4♣, 6♣, 8♣, 10♣, Q♣\n", a.getGameState());
+            "C8: 2♣, 4♣, 6♣, 8♣, 10♣, Q♣", a.getGameState());
     assertNotEquals(a.getGameState(), b.getGameState());
   }
 
   @Test
   public void move() {
 
-    //System.out.println(gameOne.getGameState());
-    gameOne.move(PileType.CASCADE, 3, 6, PileType.FOUNDATION, 0);
-    //System.out.println(gameOne.getGameState());
-    gameOne.move(PileType.FOUNDATION, 0, 0, PileType.FOUNDATION, 1);
-    //System.out.println(gameOne.getGameState());
-    gameOne.move(PileType.CASCADE, 2, 6, PileType.FOUNDATION, 1);
-    //System.out.println(gameOne.getGameState());
+    // Illegal argument: invalid cascade source pile number
+    try {
+      gameOne.move(PileType.CASCADE, -1, 6, PileType.FOUNDATION, 1);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
 
+    // Illegal argument: invalid open source pile number
+    try {
+      gameOne.move(PileType.OPEN, 5, 6, PileType.FOUNDATION, 1);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // Illegal argument: invalid foundation source pile number
+    try {
+      gameOne.move(PileType.FOUNDATION, 5, 6, PileType.FOUNDATION, 1);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // Illegal argument: invalid open dest pile number
+    try {
+      gameOne.move(PileType.OPEN, 2, 6, PileType.OPEN, 9);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // Illegal argument: invalid foundation dest pile number
+    try {
+      gameOne.move(PileType.OPEN, 3, 6, PileType.FOUNDATION, -4);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // Illegal argument: invalid cascade dest pile number
+    try {
+      gameOne.move(PileType.OPEN, 0, 6, PileType.CASCADE, 101);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // invalid move: move a card from an empty list.
+    try {
+      gameOne.move(PileType.OPEN, 0, 6, PileType.FOUNDATION, 1);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // move an A to empty foundation pile
+    gameOne.move(PileType.CASCADE, 3, 6, PileType.FOUNDATION, 0);
+    assertEquals("F1: A♦\n" +
+            "F2:\n" +
+            "F3:\n" +
+            "F4:\n" +
+            "O1:\n" +
+            "O2:\n" +
+            "O3:\n" +
+            "O4:\n" +
+            "C1: K♠, 5♠, 10♥, 2♥, 7♣, Q♦, 4♦\n" +
+            "C2: Q♠, 4♠, 9♥, A♥, 6♣, J♦, 3♦\n" +
+            "C3: J♠, 3♠, 8♥, K♣, 5♣, 10♦, 2♦\n" +
+            "C4: 10♠, 2♠, 7♥, Q♣, 4♣, 9♦\n" +
+            "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
+            "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
+            "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
+            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦", gameOne.getGameState());
+
+    // move an A from a open pile to another foundation pile
+    gameOne.move(PileType.FOUNDATION, 0, 0, PileType.FOUNDATION, 1);
+    assertEquals("F1:\n" +
+            "F2: A♦\n" +
+            "F3:\n" +
+            "F4:\n" +
+            "O1:\n" +
+            "O2:\n" +
+            "O3:\n" +
+            "O4:\n" +
+            "C1: K♠, 5♠, 10♥, 2♥, 7♣, Q♦, 4♦\n" +
+            "C2: Q♠, 4♠, 9♥, A♥, 6♣, J♦, 3♦\n" +
+            "C3: J♠, 3♠, 8♥, K♣, 5♣, 10♦, 2♦\n" +
+            "C4: 10♠, 2♠, 7♥, Q♣, 4♣, 9♦\n" +
+            "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
+            "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
+            "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
+            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦", gameOne.getGameState());
+    gameOne.move(PileType.CASCADE, 2, 6, PileType.FOUNDATION, 1);
+    assertEquals("F1:\n" +
+            "F2: A♦, 2♦\n" +
+            "F3:\n" +
+            "F4:\n" +
+            "O1:\n" +
+            "O2:\n" +
+            "O3:\n" +
+            "O4:\n" +
+            "C1: K♠, 5♠, 10♥, 2♥, 7♣, Q♦, 4♦\n" +
+            "C2: Q♠, 4♠, 9♥, A♥, 6♣, J♦, 3♦\n" +
+            "C3: J♠, 3♠, 8♥, K♣, 5♣, 10♦\n" +
+            "C4: 10♠, 2♠, 7♥, Q♣, 4♣, 9♦\n" +
+            "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
+            "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
+            "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
+            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦", gameOne.getGameState());
+
+    // invalid move: move a invalid card to a non-empty foundation pile
     try {
       gameOne.move(PileType.CASCADE, 0, 6, PileType.FOUNDATION, 1);
       fail("The move should be invalid");
@@ -121,6 +225,7 @@ public class FreecellModelTest {
       // do nothing.
     }
 
+    // invalid move: move a card in the middle.
     try {
       gameOne.move(PileType.CASCADE, 0, 3, PileType.FOUNDATION, 1);
       fail("The move should be invalid");
@@ -128,14 +233,65 @@ public class FreecellModelTest {
       // do nothing.
     }
 
-    gameOne.move(PileType.CASCADE, 1, 6, PileType.FOUNDATION, 1);
-    //System.out.println(gameOne.getGameState());
-
-    gameOne.move(PileType.CASCADE, 7, 5, PileType.OPEN, 0);
-    //System.out.println(gameOne.getGameState());
-
+    //invalid move: move a non_A card to a empty foundation pile.
     try {
-      gameOne.move(PileType.CASCADE, 6, 5, PileType.OPEN, 0);
+      gameOne.move(PileType.CASCADE, 0, 4, PileType.FOUNDATION, 3);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    // move a card to an open pile
+    gameOne.move(PileType.CASCADE, 1, 6, PileType.OPEN, 1);
+    assertEquals("F1:\n" +
+            "F2: A♦, 2♦\n" +
+            "F3:\n" +
+            "F4:\n" +
+            "O1:\n" +
+            "O2: 3♦\n" +
+            "O3:\n" +
+            "O4:\n" +
+            "C1: K♠, 5♠, 10♥, 2♥, 7♣, Q♦, 4♦\n" +
+            "C2: Q♠, 4♠, 9♥, A♥, 6♣, J♦\n" +
+            "C3: J♠, 3♠, 8♥, K♣, 5♣, 10♦\n" +
+            "C4: 10♠, 2♠, 7♥, Q♣, 4♣, 9♦\n" +
+            "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
+            "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
+            "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
+            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦", gameOne.getGameState());
+
+    // invalid move: move a cart to a non-empty open pile
+    try {
+      gameOne.move(PileType.CASCADE, 7, 5, PileType.OPEN, 1);
+      fail("The move should be invalid");
+    } catch (IllegalArgumentException e) {
+      // do nothing.
+    }
+
+    gameOne.move(PileType.CASCADE, 3, 5, PileType.OPEN, 0);
+
+    // move a valid card to cascade pile
+    gameOne.move(PileType.OPEN, 1, 0, PileType.CASCADE, 3);
+    assertEquals("F1:\n" +
+            "F2: A♦, 2♦\n" +
+            "F3:\n" +
+            "F4:\n" +
+            "O1: 9♦\n" +
+            "O2:\n" +
+            "O3:\n" +
+            "O4:\n" +
+            "C1: K♠, 5♠, 10♥, 2♥, 7♣, Q♦, 4♦\n" +
+            "C2: Q♠, 4♠, 9♥, A♥, 6♣, J♦\n" +
+            "C3: J♠, 3♠, 8♥, K♣, 5♣, 10♦\n" +
+            "C4: 10♠, 2♠, 7♥, Q♣, 4♣, 3♦\n" +
+            "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
+            "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
+            "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
+            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦", gameOne.getGameState());
+
+    // invalid move: move a card to a non-empty cascade pile
+    try {
+      gameOne.move(PileType.CASCADE, 7, 5, PileType.CASCADE, 1);
       fail("The move should be invalid");
     } catch (IllegalArgumentException e) {
       // do nothing.
@@ -180,7 +336,7 @@ public class FreecellModelTest {
             "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
             "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
             "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
-            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦\n", gameOne.getGameState());
+            "C8: 6♠, J♥, 3♥, 8♣, K♦, 5♦", gameOne.getGameState());
     gameOne.move(PileType.CASCADE, 3, 6, PileType.FOUNDATION, 0);
     gameOne.move(PileType.FOUNDATION, 0, 0, PileType.FOUNDATION, 1);
     gameOne.move(PileType.CASCADE, 2, 6, PileType.FOUNDATION, 1);
@@ -201,7 +357,7 @@ public class FreecellModelTest {
             "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
             "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
             "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
-            "C8: 6♠, J♥, 3♥, 8♣, K♦\n", gameOne.getGameState());
+            "C8: 6♠, J♥, 3♥, 8♣, K♦", gameOne.getGameState());
     try {
       gameOne.move(PileType.CASCADE, 6, 5, PileType.OPEN, 0);
       fail("The move should be invalid");
@@ -223,7 +379,7 @@ public class FreecellModelTest {
             "C5: 9♠, A♠, 6♥, J♣, 3♣, 8♦\n" +
             "C6: 8♠, K♥, 5♥, 10♣, 2♣, 7♦\n" +
             "C7: 7♠, Q♥, 4♥, 9♣, A♣, 6♦\n" +
-            "C8: 6♠, J♥, 3♥, 8♣, K♦\n", gameOne.getGameState());
+            "C8: 6♠, J♥, 3♥, 8♣, K♦", gameOne.getGameState());
   }
 
   @Test
